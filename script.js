1f5acca5b1b194e1825cdab9881f5603be2518f8
Angad <80452785+Angad-Godara@users.noreply.github.com>
2023-11-21 05:07:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select the div based on the data-cf attribute
+const divElement = document.querySelector('[data-cf]');
+
+// Check if the element was found
+if (divElement) {
+    divElement.classList.add("gs__widget__wrapper")
+    // Perform actions on the selected divElement
+    // Insert HTML content inside the selected div
+    divElement.innerHTML += '<h2 class="gs__widget__heading">CARBON FOOTPRINT TRACKER</h2>';
+    divElement.innerHTML += '<p class="gs__widget__content">Mehr Transparenz für fundiertere Entscheidungen: wir wollen Produkte mit dem geringstmöglichen CO2-Fußabdruck anbieten. Zusammen mit Carbonfact haben wir den Footprint unserer Jeans ermittelt.</p>';
+}
