c94cca71be603c8ed46fe63aa9cd97117cc74f51
Angad <80452785+Angad-Godara@users.noreply.github.com>
2023-11-21 05:18:59 +0530

    minor bug fix

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ if (divElement) {
     // Perform actions on the selected divElement
     // Insert HTML content inside the selected div
     divElement.innerHTML += '<h2 class="gs__widget__heading">CARBON FOOTPRINT TRACKER</h2>';
-    divElement.innerHTML += '<img class="gs__widget__test__img" src="./assets/IMG_7564.JPG" />';
+    divElement.innerHTML += '<img class="gs__widget__test__img" src="https://angad-godara.github.io/widget/assets/IMG_7564.JPG" />';
     divElement.innerHTML += '<p class="gs__widget__content">Mehr Transparenz für fundiertere Entscheidungen: wir wollen Produkte mit dem geringstmöglichen CO2-Fußabdruck anbieten. Zusammen mit Carbonfact haben wir den Footprint unserer Jeans ermittelt.</p>';
 }
